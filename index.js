3e1f39a44812cc071fb276184e3ebc965d9c7fa1
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-11-06 01:35:07 -0800

    ready for deployment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // code away
+require("dotenv").config();
+
 const server = require("./server");
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, (req, res) => {
   console.log(`\n*** Server Running on Port ${port} ***\n`);
